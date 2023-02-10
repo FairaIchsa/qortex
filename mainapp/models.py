75bfb76516c7ff35711914210d80014e2a4c7949
FairaIchsa <zelov02@yandex.ru>
2023-02-11 00:31:20 +0300

    updated models

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,18 +4,31 @@ from django.db import models
 class Track(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"({self.id}) {self.name}"
+
 
 class Singer(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Album(models.Model):
+    name = models.CharField(max_length=255)
     singer = models.ForeignKey(Singer, related_name='albums', on_delete=models.CASCADE)
     year = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.singer}"
 
 
 class Song(models.Model):
     song = models.ForeignKey(Track, on_delete=models.CASCADE)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, related_name='songs', on_delete=models.CASCADE)
     number = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.number}.{self.song.name} ({self.album})"
 
